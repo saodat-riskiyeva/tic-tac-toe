25c877ada4597fc766f98c5af53d9dd222c3abda
SR <saodat.riskiyeva@gmail.com>
2025-08-27 18:09:42 -0400

    handling form submission with FormData

--- scripts/config.js
+++ scripts/config.js
@@ -7,3 +7,10 @@ function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
 }
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("username");
+  console.log(enteredPlayerName);
+}
