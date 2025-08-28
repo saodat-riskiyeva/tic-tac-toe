1cf2f2d3f516dd233d0c7544715f17363a4a70bc
SR <saodat.riskiyeva@gmail.com>
2025-08-27 23:58:39 -0400

    validating user input

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -6,11 +6,20 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
+
   const formData = new FormData(event.target);
-  const enteredPlayerName = formData.get("username");
-  console.log(enteredPlayerName);
+  const enteredPlayerName = formData.get("username").trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+  }
+
+  return;
 }
